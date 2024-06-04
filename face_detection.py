89c6cc456b980e4781aa9050c988559315c82d8f
Banish J <166292800+banishjeffi@users.noreply.github.com>
2024-06-04 12:49:05 +0530

    Update face_detection.py

--- face_detection.py
+++ face_detection.py
@@ -14,7 +14,21 @@ while True:
     face = haar_cascade.detectMultiScale(grayImg,1.3,4)
 
     for (x,y,w,h) in face:
-        cv2.rectangle(img, (x,y), (x+w,y+h), (0,0,255),2)
+        
+        x1,y1=x+w, y+h
+
+        cv2.line(img, (x,y), (x+30, y),(0, 255, 0), 6)
+        cv2.line(img, (x,y), (x, y+30),(0, 255, 0), 6)
+
+        cv2.line(img, (x1,y), (x1-30, y),(0, 255, 0), 6)
+        cv2.line(img, (x1,y), (x1, y+30),(0, 255, 0), 6)
+
+        cv2.line(img, (x,y1), (x+30, y1),(0, 255, 0), 6)
+        cv2.line(img, (x,y1), (x, y1-30),(0, 255, 0), 6)
+
+        cv2.line(img, (x1,y1), (x1-30, y1),(0, 255, 0), 6)
+        cv2.line(img, (x1,y1), (x1, y1-30),(0, 255, 0), 6)
+        
     cv2.imshow("Face Detection",img)
 
     key = cv2.waitKey(10)
